1f60702efd6337c2ab1883fc4ac9ac21e62bb0d0
Edem <edem.ablaev57@yandex.ru>
2025-08-31 15:01:13 +0300

    server branch

--- src/components/MovieCardList/index.jsx
+++
@@ -1,1 +0,0 @@
-export { MovieCardList } from "./MovieCardList"
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,5 +0,0 @@
-import cls from "./NotFoundPage.module.css"
-
-export const NotFoundPage = () => {
-    return <h2 className={cls.title}> Page Not Found </h2>
-}
